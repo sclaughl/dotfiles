fe2106c89927d8fc875f41839d39746f4f8fdbc2
Stuart Laughlin <stuart@bistrotech.net>
2011-12-05 19:48:31 +0000

    Added some handy aliases (e.g. ll) and set shell to vi-mode.

--- bash.d/050_misc_config.sh
+++ bash.d/050_misc_config.sh
@@ -66,7 +66,13 @@ if [ -x /usr/bin/dircolors ]; then
     alias grep='grep --color=auto'
     alias fgrep='fgrep --color=auto'
     alias egrep='egrep --color=auto'
+    alias rgrep='rgrep --color=auto'
 fi
+
+alias ll='ls -lF'
+alias la='ls -A'
+
+set -o vi
 
 #############################################################################
 # }}}   
